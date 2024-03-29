57215c25f4c7747820d4e9993dea2aa262bdd702
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-03-29 14:56:31 +0800

    Update server.go

--- app/server.go
+++ app/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -20,7 +21,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	response := "HTTP/1.1 200 OK\r\n\r\n"
+	request_bytes := make([]byte, 1024)
+	_, err = conn.Read(request_bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	request := string(request_bytes)
+	lines := strings.Split(request, "\r\n")
+	path := strings.Split(lines[0], " ")[1]
+
+	var response string
+	if path == "/" {
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		response = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+	}
+
 	_, err = conn.Write([]byte(response))
 	if err != nil {
 		log.Fatal(err)
