dd8dce9d09553cb3327a8559180cde064422e5ae
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-03-29 21:51:54 +0800

    yes

--- app/server.go
+++ app/server.go
@@ -11,30 +11,65 @@ import (
 	"strings"
 )
 
+type Status struct {
+	OK                    string
+	CREATED               string
+	NOT_FOUND             string
+	INTERNAL_SERVER_ERROR string
+}
+
+var StatusCode = Status{
+	OK:                    "HTTP/1.1 200 OK",
+	CREATED:               "HTTP/1.1 201 CREATED",
+	NOT_FOUND:             "HTTP/1.1 404 NOT_FOUND",
+	INTERNAL_SERVER_ERROR: "HTTP/1.1 500 INTERNAL_SERVER_ERROR",
+}
+
+type Content struct {
+	text string
+	file string
+}
+
+var ContentType = Content{
+	text: "\r\nContent-Type: text/plain\r\n",
+	file: "\r\nContent-Type: application/octet-stream\r\n",
+}
+
 var directory string
 
 func main() {
-	flag.StringVar(&directory, "directory", "", "")
+	flag.StringVar(&directory, "directory", "", "directory to read and write files")
 	flag.Parse()
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	host, port := "127.0.0.1", "4221"
+	address := host + ":" + port
+
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Failed to bind to port", port)
 		os.Exit(1)
 	}
-	defer l.Close()
+	defer listener.Close()
+	fmt.Println("Listening on http://" + listener.Addr().String())
 
 	for {
-		conn, err := l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handler(conn)
+		go router(conn)
 	}
 }
 
-func handler(conn net.Conn) {
+type Request struct {
+	method     string
+	path       string
+	user_agent string
+	body       string
+}
+
+func router(conn net.Conn) {
 	request_bytes := make([]byte, 1024)
 	n, err := conn.Read(request_bytes)
 	if err != nil {
@@ -42,80 +77,104 @@ func handler(conn net.Conn) {
 	}
 	request := string(request_bytes)
 	lines := strings.Split(request[:n], "\r\n")
+
 	start_line := lines[0]
 	start_line_parts := strings.Split(start_line, " ")
 	method := start_line_parts[0]
-	path := start_line_parts[1]
-
-	var response string
-	if path == "/" {
-		response = "HTTP/1.1 200 OK\r\n\r\n"
-	} else if strings.HasPrefix(path, "/echo/") {
-		body, _ := strings.CutPrefix(path, "/echo/")
-		len := strconv.Itoa(len(body))
-
-		response = "HTTP/1.1 200 OK\r\n" +
-			"Content-Type: text/plain\r\n" +
-			"Content-Length: " + len + "\r\n\r\n" +
-			body + "\r\n\r\n"
-	} else if path == "/user-agent" {
-		var user_agent string
-		for _, line := range lines {
-			if strings.HasPrefix(line, "User-Agent: ") {
-				user_agent, _ = strings.CutPrefix(line, "User-Agent: ")
-			}
-		}
-		len := strconv.Itoa(len(user_agent))
-
-		response = "HTTP/1.1 200 OK\r\n" +
-			"Content-Type: text/plain\r\n" +
-			"Content-Length: " + len + "\r\n\r\n" +
-			user_agent + "\r\n\r\n"
-	} else if strings.HasPrefix(path, "/files/") && method == "GET" {
-		file_name, _ := strings.CutPrefix(path, "/files/")
-		file_name, err = url.QueryUnescape(file_name)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		file, err := os.Open(directory + "/" + file_name)
-		if err != nil {
-			response = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
-		} else {
-			content := make([]byte, 1024)
-			bytes_length, err := file.Read(content)
-			if err != nil {
-				log.Fatal(err)
-			}
-			len := strconv.Itoa(bytes_length)
-			body := string(content)
-
-			response = "HTTP/1.1 200 OK\r\n" +
-				"Content-Type: application/octet-stream\r\n" +
-				"Content-Length: " + len + "\r\n\r\n" +
-				body + "\r\n\r\n"
-		}
-		file.Close()
-	} else if strings.HasPrefix(path, "/files/") && method == "POST" {
-		file_name, _ := strings.CutPrefix(path, "/files/")
-		file_name, err = url.QueryUnescape(file_name)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = os.WriteFile(directory+"/"+file_name, []byte(lines[len(lines)-1]), 0666)
-		if err != nil {
-			log.Fatal(err)
-		}
-		response = "HTTP/1.1 201 CREATED\r\n\r\n"
-	} else {
-		response = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
-	}
-
-	_, err = conn.Write([]byte(response))
+	path, err := url.QueryUnescape(start_line_parts[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	var user_agent string
+	for _, line := range lines {
+		if strings.HasPrefix(line, "User-Agent: ") {
+			user_agent, _ = strings.CutPrefix(line, "User-Agent: ")
+			break
+		}
+	}
+
+	body := lines[len(lines)-1]
+
+	r := Request{
+		method,
+		path,
+		user_agent,
+		body,
+	}
+
+	var response string
+
+	if path == "/" && method == "GET" {
+		response = get_health()
+	}
+	if path == "/user-agent" && method == "GET" {
+		response = get_user_agent(r)
+	}
+	if strings.HasPrefix(path, "/files/") && method == "GET" {
+		response = get_file(r)
+	}
+	if strings.HasPrefix(path, "/files/") && method == "POST" {
+		response = post_file(r)
+	}
+	if response == "" {
+		response = StatusCode.NOT_FOUND
+	}
+
+	_, err = conn.Write([]byte(response + "\r\n\r\n"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	conn.Close()
+}
+
+func get_health() string {
+	return StatusCode.OK
+}
+
+func get_user_agent(r Request) string {
+	content_length := strconv.Itoa(len(r.user_agent))
+
+	return StatusCode.OK +
+		ContentType.text +
+		"Content-Length: " + content_length +
+		"\r\n\r\n" +
+		r.user_agent
+}
+
+func get_file(r Request) string {
+	file_name, _ := strings.CutPrefix(r.path, "/files/")
+	file, err := os.Open(directory + "/" + file_name)
+	if err != nil {
+		return StatusCode.NOT_FOUND
+	}
+
+	file_content := make([]byte, 1024)
+	n, err := file.Read(file_content)
+	if err != nil {
+		fmt.Println(err)
+		return StatusCode.INTERNAL_SERVER_ERROR
+	}
+
+	content_length := strconv.Itoa(n)
+	body := string(file_content[:n])
+
+	file.Close()
+	return StatusCode.OK +
+		ContentType.file +
+		"Content-Length: " + content_length +
+		"\r\n\r\n" +
+		body
+}
+
+func post_file(r Request) string {
+	file_name, _ := strings.CutPrefix(r.path, "/files/")
+
+	err := os.WriteFile(directory+"/"+file_name, []byte(r.body), 0666)
+	if err != nil {
+		fmt.Println(err)
+		return StatusCode.INTERNAL_SERVER_ERROR
+	}
+	return StatusCode.CREATED
 }
